0d48cd066558dfd26cae7a93e7ee2d9bd6e4cdbe
Mark <h091237557@gmail.com>
2020-05-26 16:27:52 +0800

    Modify: modify the base62 size limit.

--- libs/base62-parser.js
+++ libs/base62-parser.js
@@ -1,19 +1,21 @@
 
 /* DEF: a ~ z + A ~ Z + 0~6 = 62 */
 const SHUFFLED_CHARS62 = "IkKBsnXz3RVhqcC2E6mwLbGPaTpWeZMvO4iu09SF5QtUjdNoxf7Y1DlHgAyJ8r";
+const MIN_LETTER_SIZE = 2;
 
 module.exports = {
     /**
-     * @param {Integer} number : 
+     * @param {Integer} number 
      * @return {String}  
      */
     encode : (number) => {
         let res = "";
-        number *= 62;
-        while(number > 0){
+        let size = MIN_LETTER_SIZE;
+        while(number != 0 || size > 0){
             let digit = number % 62;
             res = SHUFFLED_CHARS62[digit] + res;
             number = Math.floor(number / 62);
+            size--;
         }
         return res;
     }
